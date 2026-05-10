2a731f1115488a47c997cd53e277dbc139f63b61
Lalasa <lalasa0613@gmail.com>
2026-05-10 19:50:54 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,15 +1,34 @@
+import streamlit as st
 import firebase_admin
 
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+# -----------------------------------------
 
+# PREVENT MULTIPLE INITIALIZATIONS
+
+# -----------------------------------------
+
+if not firebase_admin._apps:
+
+```
 cred = credentials.Certificate(
-    "firebase_key.json"
+
+    dict(
+        st.secrets["firebase"]
+    )
 )
 
 firebase_admin.initialize_app(
     cred
 )
+```
+
+# -----------------------------------------
+
+# FIRESTORE CLIENT
+
+# -----------------------------------------
 
 db = firestore.client()
